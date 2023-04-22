46228cf745050743e6cb094f8e661c5975457cb0
NAVEEN KUMAR REDDY Y <93367965+nkr2003@users.noreply.github.com>
2023-04-22 11:55:57 +0530

    Update LFU.java

--- LFU.java
+++ LFU.java
@@ -1,58 +1,55 @@
 import java.util.*;
-class LFUPageReplacement {
+
+public class LFUPageReplacement {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the number of frames: ");
+        int numFrames = scanner.nextInt();
+        
+        int[] frames = new int[numFrames];
+        int[] counts = new int[numFrames];
+        int numFaults = 0;
+        
         System.out.print("Enter the number of pages: ");
-        int n = sc.nextInt();
-
-        int[] pages = new int[n];
-        System.out.println("Enter the page reference string:");
-        for (int i = 0; i < n; i++) {
-            pages[i] = sc.nextInt();
+        int numPages = scanner.nextInt();
+        int[] pages = new int[numPages];
+        
+        System.out.println("Enter the pages:");
+        for (int i = 0; i < numPages; i++) {
+            pages[i] = scanner.nextInt();
         }
-
-        System.out.print("Enter the number of page frames: ");
-        int capacity = sc.nextInt();
-
-        int[] freq = new int[capacity];
-        int[] frame = new int[capacity];
-        Arrays.fill(frame, -1);
-        int pageFaults = 0;
-
-        for (int i = 0; i < n; i++) {
-            int j = find(frame, pages[i]);
-            if (j == -1) {
-                int k = findMin(freq);
-                frame[k] = pages[i];
-                freq[k] = 1;
-                pageFaults++;
-            } else {
-                freq[j]++;
+        
+        for (int i = 0; i < numPages; i++) {
+            int page = pages[i];
+            boolean found = false;
+            for (int j = 0; j < numFrames; j++) {
+                if (frames[j] == page) {
+                    counts[j]++;
+                    found = true;
+                    break;
+                }
             }
-        }
-
-        System.out.println("Number of page faults: " + pageFaults);
-    }
-
-    public static int find(int[] frame, int page) {
-        for (int i = 0; i < frame.length; i++) {
-            if (frame[i] == page) {
-                return i;
+            if (!found) {
+                int minCount = counts[0];
+                int minIndex = 0;
+                for (int j = 1; j < numFrames; j++) {
+                    if (counts[j] < minCount) {
+                        minCount = counts[j];
+                        minIndex = j;
+                    }
+                }
+                frames[minIndex] = page;
+                counts[minIndex] = 1;
+                numFaults++;
             }
-        }
-        return -1;
-    }
-
-    public static int findMin(int[] freq) {
-        int min = freq[0];
-        int minIndex = 0;
-        for (int i = 1; i < freq.length; i++) {
-            if (freq[i] < min) {
-                min = freq[i];
-                minIndex = i;
+            System.out.print("Page References: ");
+            for (int j = 0; j < numFrames; j++) {
+                System.out.print(frames[j] + " ");
             }
+            System.out.println();
         }
-        return minIndex;
+        
+        System.out.println("Number of page faults: " + numFaults);
     }
 }
